17bce37e57f5e0f1a105a2fed5c186f4c801f761
SpoonFll <101011715+SpoonFll@users.noreply.github.com>
2022-04-22 23:43:22 -0400

    Add files via upload
    changed source code for proper keyboard support

--- popKernel/kernel.c
+++ popKernel/kernel.c
@@ -32,30 +32,154 @@
 #define KEY_Y 0x15
 #define KEY_Z 0x2C
 #define KEY_ENTER 0x1C
+#define KEY_1 0x02
+#define KEY_2 0x03
+#define KEY_3 0x04
+#define KEY_4 0x05
+#define KEY_5 0x06
+#define KEY_6 0x07
+#define KEY_7 0x08
+#define KEY_8 0x09
+#define KEY_9 0x0A
+#define KEY_0 0x0B
+#define KEY_MINUS 0x0C
+#define KEY_EQUAL 0x0D
+#define KEY_SQUARE_OPEN_BRACKET 0x1A
+#define KEY_SQUARE_CLOSE_BRACKET 0x1B
+#define KEY_SEMICOLON 0x27
+#define KEY_BACKSLASH 0x2B
+#define KEY_COMMA 0x33
+#define KEY_DOT 0x34
+#define KEY_FORESLASH 0x35
+#define KEY_F1 0x3B
+#define KEY_F2 0x3C
+#define KEY_F3 0x3D
+#define KEY_F4 0x3E
+#define KEY_F5 0x3F
+#define KEY_F6 0x40
+#define KEY_F7 0x41
+#define KEY_F8 0x42
+#define KEY_F9 0x43
+#define KEY_F10 0x44
+#define KEY_F11 0x85
+#define KEY_F12 0x86
+#define KEY_BACKSPACE 0x0E
+#define KEY_DELETE 0x53
+#define KEY_DOWN 0x50
+#define KEY_END 0x4F
+#define KEY_ESC 0x01
+#define KEY_HOME 0x47
+#define KEY_INSERT 0x52
+#define KEY_KEYPAD_5 0x4C
+#define KEY_KEYPAD_MUL 0x37
+#define KEY_KEYPAD_Minus 0x4A
+#define KEY_KEYPAD_PLUS 0x4E
+#define KEY_KEYPAD_DIV 0x35
+#define KEY_LEFT 0x4B
+#define KEY_PAGE_DOWN 0x51
+#define KEY_PAGE_UP 0x49
+#define KEY_PRINT_SCREEN 0x37
+#define KEY_RIGHT 0x4D
+#define KEY_SPACE 0x39
+#define KEY_TAB 0x0F
+#define KEY_UP 0x48
 
 #define BLACK 0
 #define GREEN 2
 #define RED 4
 #define YELLOW 14
 #define WHITE_COLOR 15
+
 #endif
+
 unsigned short *terminal_buffer;
 
 unsigned int vga_index;
 unsigned int lineNumber;
+
 char get_ascii_char(int ascii)
 {
 	switch(ascii)
 	{
-		case KEY_Q:
-		     return 'q';
+		case KEY_A:
+		     return 'a';
 		     break;
-		case KEY_W:
-		     return 'w';
+		case KEY_B:
+		     return 'b';
+		     break;
+		case KEY_C:
+		     return 'c';
+		     break;
+		case KEY_D:
+		     return 'd';
 		     break;
 		case KEY_E:
 		     return 'e';
 		     break;
+		case KEY_F:
+		     return 'f';
+		     break;	
+		case KEY_G:
+		     return 'g';
+		     break;
+		case KEY_H:
+		     return 'h';
+		     break;
+		case KEY_I:
+		     return 'i';
+		     break;
+		case KEY_J:
+		     return 'j';
+		     break;
+		case KEY_K:
+		     return 'k';
+		     break;
+		case KEY_L:
+		     return 'l';
+		     break;
+		case KEY_M:
+		     return 'm';
+		     break;
+		case KEY_N:
+		     return 'n';
+		     break;
+		case KEY_O:
+		     return 'o';
+		     break;
+		case KEY_P:
+		     return 'p';
+		     break;
+		case KEY_Q:
+		     return 'q';
+		     break;
+		case KEY_R:
+		     return 'r';
+		     break;
+		case KEY_S:
+		     return 's';
+		     break;
+		case KEY_T:
+		     return 't';
+		     break;
+		case KEY_U:
+		     return 'u';
+		     break;	
+		case KEY_V:
+		     return 'v';
+		     break;
+		case KEY_W:
+		     return 'w';
+		     break;
+		case KEY_X:
+		     return 'x';
+		     break;
+		case KEY_Y:
+		     return 'y';
+		     break;
+		case KEY_Z:
+		     return 'z';
+		     break;
+		return (char)0;
 
 	}
 }
@@ -68,6 +192,11 @@ void print_string(char *str,unsigned char color)
 		index++;
 		vga_index++;
 	}
+}
+void print_character(char c, unsigned char color)
+{
+	terminal_buffer[vga_index] = (unsigned short)c|(unsigned short)color<<8;
+	vga_index++;
 }
 void next_line(void)
 {
@@ -118,10 +247,11 @@ void test_input(void)
 		else
 		{
 			ch = get_ascii_char(keycode);
-			char* character = ch;
-			print_string(character,GREEN);
+			
+
+			print_character(ch,GREEN);
 		}
-		wait_for_io(0x02FFFFFF);
+		wait_for_io(0x03FFFFFF);
 	}while(ch>-1);
 
 }
